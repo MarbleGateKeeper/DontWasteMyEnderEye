01db8fb9a058c1b1f165255476dacd61849c8b59
白门守望者 <marblegatekeeper@outlook.com>
2022-04-02 21:47:04 +0800

    Update to 1.1.1
    [update] Support change broken ender eye warping probability in config.

--- src/main/java/love/marblegate/homingendereye/misc/Configuration.java
+++ src/main/java/love/marblegate/homingendereye/misc/Configuration.java
@@ -8,6 +8,7 @@ public class Configuration {
     public static ForgeConfigSpec.BooleanValue INDIVIDUAL_MODE;
     public static ForgeConfigSpec.IntValue SCANNING_RATE;
     public static ForgeConfigSpec.IntValue SCANNING_RADIUS;
+    public static ForgeConfigSpec.DoubleValue WARPING_PROBABILITY;
 
     static {
         ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
@@ -16,6 +17,7 @@ public class Configuration {
         INDIVIDUAL_MODE = builder.comment("Should Ender Eye Destroy Event Only Belongs to Specific Player?").define("INDIVIDUAL_MODE", false);
         SCANNING_RATE = builder.comment("Scanning interval for nearby End Portal Frame. The unit is tick.").defineInRange("SCANNING_RATE", 300, 1, Integer.MAX_VALUE);
         SCANNING_RADIUS = builder.comment("Scanning Radius for nearby End Portal Frame.").defineInRange("SCANNING_RATE", 30, 1, 160);
+        WARPING_PROBABILITY = builder.comment("The Probability of broken ender eye warping").defineInRange("WARPING_PROBABILITY", 1.0, 0, 1.0);
         builder.pop();
 
         MOD_CONFIG = builder.build();
