1a63f553db8572f27d0fa940f1bb2a229f53b3b9
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-07-17 16:02:02 +0800

    Pump forge to 41.0.99

--- src/main/java/love/marblegate/homingendereye/event/CreateCacheHandler.java
+++ src/main/java/love/marblegate/homingendereye/event/CreateCacheHandler.java
@@ -2,14 +2,14 @@ package love.marblegate.homingendereye.event;
 
 import love.marblegate.homingendereye.HomingEnderEye;
 import love.marblegate.homingendereye.misc.EyeThrowCache;
-import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class CreateCacheHandler {
     @SubscribeEvent
-    public static void load(WorldEvent.Load event){
+    public static void load(LevelEvent.Load event){
         if(HomingEnderEye.EYE_THROW_CACHE == null){
             HomingEnderEye.EYE_THROW_CACHE = new EyeThrowCache();
         }
